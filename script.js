babe98e6fe0ff6fe98111d39d1baf4375b0caa8a
chrissundal <christoffersj@hotmail.com>
2024-09-05 14:48:22 +0200

    lagt til litt moro

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 let lettersPressed = '';
+let startMessage = '';
+let funElement = '';
+let powerOn = false;
 
-        updateView();
-        function updateView(){
-            let html = `
+updateView();
+function updateView() {
+    let html = `
                 <div class="ramme">
-                        <div class="tekst">Tekst: ${lettersPressed ?? ''}</div>
+                        <div class="tekst">${startMessage} ${lettersPressed ?? ''}${funElement}</div>
                     <div class="btnframe">
                         <button class="restart" onclick="stopColor()"><strong>Reset DOS</strong></button>
+                        <button class="start" onclick="powerPc()"><strong>Power</strong></button>
                     </div>
                 </div>
             <div class="rammeTast">
@@ -17,10 +21,10 @@ let lettersPressed = '';
                 </div>
             </div>
             `;
-            document.getElementById('app').innerHTML = html;
-        }
-        function createFirstTast(){
-        return  `<div class="rad1">
+    document.getElementById('app').innerHTML = html;
+}
+function createFirstTast() {
+    return `<div class="rad1">
                     <button onclick="tastatur(this)">Q</button>
                     <button onclick="tastatur(this)">W</button>
                     <button onclick="tastatur(this)">E</button>
@@ -35,10 +39,10 @@ let lettersPressed = '';
                     <button onclick="tastatur(this)">¨</button>
                 </div> 
             `;
-            updateView();
-        }
-        function createSecondTast(){
-            return  `<div class="rad2">
+
+}
+function createSecondTast() {
+    return `<div class="rad2">
                 <button onclick="tastatur(this)">A</button>
                 <button onclick="tastatur(this)">S</button>
                 <button onclick="tastatur(this)">D</button>
@@ -52,10 +56,10 @@ let lettersPressed = '';
                 <button onclick="tastatur(this)">Æ</button>
                 </div>
                 `;
-                updateView();
-            }
-            function createThirdTast(){
-                return  `<div class="rad3">
+
+}
+function createThirdTast() {
+    return `<div class="rad3">
                     <button onclick="tastatur(this)">Z</button>
                     <button onclick="tastatur(this)">X</button>
                     <button onclick="tastatur(this)">C</button>
@@ -68,13 +72,45 @@ let lettersPressed = '';
                     <button onclick="tastatur(this)">-</button>
                     </div>
                     `;
-                    updateView();
-        }
-        
-        function tastatur(htmlElement) {
-            lettersPressed += htmlElement.innerHTML;
-            updateView();
-        }
-        function stopColor() {
-            location.reload();
-        }
+
+}
+
+function powerPc() {
+    setTimeout(firstStartUpPc, 1000)
+}
+function firstStartUpPc() {
+    startMessage = 'Boot'
+    setTimeout(secondStartUpPc, 3000)
+    updateView();
+}
+function secondStartUpPc() {
+    setTimeout(thirdStartUpPc, 3000)
+    startMessage = '...'
+    updateView();
+}
+function thirdStartUpPc() {
+    startMessage = 'C:'
+    powerOn = !powerOn;
+    firstElm();
+    updateView();
+}
+function firstElm() {
+    setTimeout(secondElm, 1000)
+    funElement = '_'
+    updateView();
+}
+function secondElm() {
+    setTimeout(firstElm, 1000)
+    funElement = ''
+    updateView();
+}
+
+function tastatur(htmlElement) {
+    if (powerOn == true) {
+        lettersPressed += htmlElement.innerHTML;
+    }
+    updateView();
+}
+function stopColor() {
+    location.reload();
+}
